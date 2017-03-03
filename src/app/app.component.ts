146157a7daf3c348437d26a4eeac4be1dedb6501
Quentin Berlemont <quentinberlemont@gmail.com>
2017-03-03 22:57:16 +0100

    feat(app): add ionic menu starter pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Nav } from 'ionic-angular';
+
+import { Page1 } from '../pages/page1/page1';
+import { Page2 } from '../pages/page2/page2';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = Page1;
+  pages: Array<{title: string, component: any}>;
+
+  constructor() {
+    this.pages = [
+      { title: 'Page One', component: Page1 },
+      { title: 'Page Two', component: Page2 }
+    ];
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,24 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 
 import { AppComponent } from './app.component';
+import { Page1 } from '../pages/page1/page1';
+import { Page2 } from '../pages/page2/page2';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1,
+    Page2
   ],
   imports: [
     IonicModule.forRoot(AppComponent)
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}],
-  bootstrap: [IonicApp]
+  bootstrap: [IonicApp],
+  entryComponents: [
+    AppComponent,
+    Page1,
+    Page2
+  ],
 })
 export class AppModule { }
